af77013889fb4ebe414bd230f42ee6225a714f7e
Holger Wingold <holger.wingold@adwizor.de>
2018-08-14 20:51:44 +0200

    Step 8

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -24,7 +24,7 @@ sap.ui.define([
         onShowHello : function() {
             // read msg from i18n model
             var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView.getModel().getProperty("/recipient/name");
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             // show message
             MessageToast.show(sMsg);
