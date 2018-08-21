7801663567474270cf00d03fc6ddb1b159470f82
Holger Wingold <holger.wingold@adwizor.de>
2018-08-21 14:04:48 +0200

    Step 16

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -11,6 +11,17 @@ sap.ui.define([
         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
         // show message
         MessageToast.show(sMsg);
+    },
+    onOpenDialog : function() {
+      var oView = this.getView();
+      var oDialog = oView.byId("helloDialog");
+      // create dialog lazily
+      if (!oDialog) {
+        // create dialog via fragment factory
+        oDialog = sap.ui.xmlfragment(oView.getId(), "sap.ui.demo.walkthrough.view.HelloDialog");
+        oView.addDependent(oDialog);
+      }
+      oDialog.open();
     }
   });
 });
