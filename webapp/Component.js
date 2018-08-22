d8a823096bba4062c8d62d7d4688b0775c1c57a6
Holger Wingold <holger.wingold@adwizor.de>
2018-08-22 11:34:01 +0200

    Step 19

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,13 +1,14 @@
 sap.ui.define([
   "sap/ui/core/UIComponent",
   "sap/ui/model/json/JSONModel",
-  "sap/ui/model/resource/ResourceModel"
-], function(UIComponent, JSONModel, ResourceModel) {
+  "sap/ui/demo/walkthrough/controller/HelloDialog"
+], function(UIComponent, JSONModel, HelloDialog) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
             metadata : {
               manifest: "json"
             },
+
       init : function() {
         // call the init function of the parent
         UIComponent.prototype.init.apply(this, arguments);
@@ -20,6 +21,19 @@ sap.ui.define([
         };
         var oModel = new JSONModel(oData);
         this.setModel(oModel);
+
+        // set dialog
+        this._helloDialog = new HelloDialog(this.getRootControl());
+      },
+
+      exit : function() {
+        this._helloDialog.destroy();
+        delete this._helloDialog;
+      },
+
+      openHelloDialog : function() {
+        this._helloDialog.open();
       }
+      
     });
 });
--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -6,29 +6,17 @@ sap.ui.define([
   return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
 
     onShowHello : function() {
-        // read msg from i18n model
-        var oBundle = this.getView().getModel("i18n").getResourceBundle();
-        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-        // show message
-        MessageToast.show(sMsg);
+      // read msg from i18n model
+      var oBundle = this.getView().getModel("i18n").getResourceBundle();
+      var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      // show message
+      MessageToast.show(sMsg);
     },
 
     onOpenDialog : function() {
-      var oView = this.getView();
-      var oDialog = oView.byId("helloDialog");
-      // create dialog lazily
-      if (!oDialog) {
-        // create dialog via fragment factory
-        oDialog = sap.ui.xmlfragment(oView.getId(), "sap.ui.demo.walkthrough.view.HelloDialog", this);
-        // connect dialog to view (models, lifecycle)
-        oView.addDependent(oDialog);
-      }
-      oDialog.open();
-    },
-
-    onCloseDialog : function() {
-      this.getView().byId("helloDialog").close();
+      this. getOwnerComponent().openHelloDialog();
     }
+
   });
 });
