d664aaf4d139c0de0f05b3d017227c281deee577
Holger Wingold <holger.wingold@adwizor.de>
2018-08-24 13:13:14 +0200

    Step 23

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/demo/walkthrough/model/formatter"
+], function (Controller, JSONModel, formatter) {
   "use strict";
 
   return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
+
+    formatter: formatter,
 
     onInit : function() {
       var oViewModel = new JSONModel({
@@ -12,6 +15,6 @@ sap.ui.define([
       });
       this.getView().setModel(oViewModel, "view");
     }
-    
+
   });
 });
