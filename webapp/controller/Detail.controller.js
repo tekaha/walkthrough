42960daa7493c31229a30e79e687ce03eb61315b
Holger Wingold <holger.wingold@adwizor.de>
2018-09-27 09:33:24 +0200

    Step 34

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/core/routing/History"
-], function (Controller, History) {
+  "sap/ui/core/routing/History",
+  "sap/m/MessageToast"
+], function (Controller, History, MessageToast) {
   "use strict";
   return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
 
@@ -11,6 +12,7 @@ sap.ui.define([
     },
 
     _onObjectMatched: function(oEvent) {
+      this.byId("rating").reset();
       this.getView().bindElement({
         path: "/" + oEvent.getParameter("arguments").invoicePath,
         model: "invoice"
@@ -27,6 +29,12 @@ sap.ui.define([
         var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
         oRouter.navTo("overview", {}, true);
       }
+    },
+
+    onRatingChange: function(oEvent) {
+      var fValue = oEvent.getParameter("value");
+      var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+      MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
     }
   });
 });
